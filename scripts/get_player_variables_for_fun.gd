8216ac84b9afcab3fd71bc4a821b5c7010639153
vomic <89675453+imawormeater@users.noreply.github.com>
2024-05-20 16:14:53 -0500

    ,

--- scripts/get_player_variables_for_fun.gd
+++ scripts/get_player_variables_for_fun.gd
@@ -1,6 +1,6 @@
 extends Label
 @export var logan:CharacterBody3D
-
+	
 func _process(delta: float) -> void:
 	var format_string = "LOGAN NEAL STATS:\nvelocity:%s\nspeed:%s\nstate:%s\naccel_mult:%s"
 	var actual_string = format_string % [logan.velocity, logan.SPEED,logan.state,logan.DEACEL_mult]
