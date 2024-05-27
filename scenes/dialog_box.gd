8e9fdc0a93983dd84e21e09ae43d18810ff80ffa
vomic <89675453+imawormeater@users.noreply.github.com>
2024-05-27 16:34:12 -0500

    tail thing is better

--- scenes/dialog_box.gd
+++ scenes/dialog_box.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
 @export var currentspeaker:Node3D = null
+@export var tailMag := 63.0
+@export var tailWidth := 20.0
+
 var visibleOnScreenforSpeaker:VisibleOnScreenNotifier3D
 
 @onready var dialogboundary := $DialogBoundary
 @onready var tail := $DialogBoundary/Frame/Tail
-var tailMag := 63.0
+var initPoint := Vector2(320,350)
+
+var hidden := true
 
 func _ready() -> void:
 	set_speaker(currentspeaker)
@@ -13,16 +18,23 @@ func _ready() -> void:
 func set_polygon(_position:Vector2,delta:float):
 	var _lerp_speed:float = 1-pow(0.000000000005,delta)
 	
-	var polygon1:Vector2 = tail.polygon[1]
-	polygon1 = polygon1.lerp(_position - tail.global_position,_lerp_speed)
+	if hidden:
+		tail.polygon[1][1] = lerpf(tail.polygon[1][1],-96,_lerp_speed)
+		return
 	
-	#polygon1.x = clampf(polygon1.x,-220.0,220.0)
-	#polygon1.y = clampf(polygon1.y,-156.0,-96.0)
-	polygon1.y = clampf(polygon1.y,-999.0,-96.0)
+	var endpoint:Vector2 = _position - tail.global_position
+	var beginpoint := Vector2(endpoint.x/1.3,-93)
+	beginpoint.y = clampf(beginpoint.y,-999.0,-96.0)
+	beginpoint.x = clampf(beginpoint.x,-225.0,225.0)
 	
-	tail.polygon[0][0] = (polygon1.x/1.5) + 20
-	tail.polygon[2][0] = (polygon1.x/1.5) - 20
-	tail.polygon[1] = polygon1
+	var vector:Vector2 = Vector2(endpoint.x-beginpoint.x,endpoint.y-beginpoint.y)
+	vector = vector.normalized() * tailMag
+	endpoint = vector + beginpoint
+	tail.polygon[1] = tail.polygon[1].lerp(endpoint,_lerp_speed)
+	
+	tail.polygon[0] = beginpoint + Vector2(tailWidth,0)
+	tail.polygon[2] = beginpoint - Vector2(tailWidth,0)
+	#tail.polygon[1] = endpoint
 
 func set_speaker(speaker:Node3D):
 	if speaker == null:
@@ -35,11 +47,15 @@ func set_speaker(speaker:Node3D):
 	
 
 func _process(delta: float) -> void:
-	var speakpos := Vector2(320,350)
+	tail.show()
+	var speakpos := initPoint
+	hidden = false
 	if currentspeaker != null:
 		speakpos = get_viewport().get_camera_3d().unproject_position(currentspeaker.global_position)
 		if visibleOnScreenforSpeaker != null:
 			if not visibleOnScreenforSpeaker.is_on_screen():
-				speakpos = Vector2(320,350)
-	#print(speakpos)
+				speakpos = initPoint
+				hidden = true
+	else:
+		hidden = true
 	set_polygon(speakpos,delta)
