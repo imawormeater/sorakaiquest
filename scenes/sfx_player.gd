214b52484fc0f82a9630d1cecf968aa23ae67e9d
vomic <89675453+imawormeater@users.noreply.github.com>
2024-05-29 21:28:05 -0500

    wall riding sound fixed

--- scenes/sfx_player.gd
+++ scenes/sfx_player.gd
@@ -6,9 +6,26 @@ func _ready() -> void:
 	for i in get_children():
 		if i is AudioStreamPlayer3D:
 			children[i.name] = i
+			
+func play_sound_into(_name:String,next_name:String) -> void:
+	var stream:AudioStreamPlayer3D = children[_name]
+	if stream == null:
+		push_warning(_name," isn't a valid child, not playing sound.")
+		return
+	var stream2:AudioStreamPlayer3D = children[next_name]
+	if stream2 == null:
+		push_warning(next_name," isn't a valid child, not playing sound.")
+		return
+	if stream.get_meta("StopOtherSounds") == true:
+		stop_all_sounds()
+	stream.play()
+	await get_tree().create_timer(stream.stream.get_length()).timeout
+	if stream.playing:
+		stream2.play()
+	
 
 func play_sound(_name:String) -> void:
-	var stream = children[_name]
+	var stream:AudioStreamPlayer3D = children[_name]
 	if stream == null:
 		push_warning(_name," isn't a valid child, not playing sound.")
 		return
@@ -20,14 +37,14 @@ func get_sound(_name:String) -> AudioStreamPlayer3D:
 	return children[_name]
 
 func set_sound_speed(_name:String,playback:float) -> void:
-	var stream = children[_name]
+	var stream:AudioStreamPlayer3D = children[_name]
 	if stream == null:
 		push_warning(_name," isn't a valid child, not setting speed.")
 		return
 	stream.pitch_scale = playback
 	
 func stop_sound(_name:String) -> void:
-	var stream = children[_name]
+	var stream:AudioStreamPlayer3D = children[_name]
 	if stream == null:
 		push_warning(_name," isn't a valid child, not stoping sound.")
 		return
