faab3b4cc4d26250ad721cd610a073b843f40f68
vomic <89675453+imawormeater@users.noreply.github.com>
2024-11-01 15:53:06 -0500

    gfhhgffgh

--- scenes/states/intro_cutscene.gd
+++ scenes/states/intro_cutscene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var gameState:PackedScene 
 @onready var video:VideoStreamPlayer = $VideoStreamPlayer
-@onready var stream:VideoStreamTheora = preload("res://assets/videos/sorakai quest intro.ogv")
+@export var stream:VideoStreamTheora = preload("res://assets/videos/sorakai quest intro.ogv")
 var bye:bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
