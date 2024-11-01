6c9f03954eae6bcde0681e88782a7b64666a4204
vomic <89675453+imawormeater@users.noreply.github.com>
2024-10-31 21:15:25 -0500

    ok i fixed again

--- scripts/albumCollectable.gd
+++ scripts/albumCollectable.gd
@@ -43,8 +43,9 @@ extends Node3D
 
 
 func _ready() -> void:
-	if id >= albumNames.size():
-		id = albumNames.size()-1
+	#if id >= albumNames.size():
+		#id = albumNames.size()-1
+	print(id)
 	albumName = albumNames[id]
 	albumMaterial["shader_parameter/Texture"] = albumTextures[id]
 	
